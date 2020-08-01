5536cebb855d9fdf3776c3a82f72c07d4597271c
LEESR <sy870203>
2020-08-01 19:22:42 +0900

    add order model

---
+++ models/order.js
@@ -0,0 +1,18 @@
+// 1
+const mongoose = require("mongoose");
+
+// 2
+const orderSchema = mongoose.Schema({
+    product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+        required: true
+    },
+    quantity: {
+        type: Number,
+        default: 1
+    }
+});
+
+// 3 
+module.exports = mongoose.model("order", orderSchema);
