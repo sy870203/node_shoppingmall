d6c9b224c69eb729fe753af86eddb32b7caf8271
LEESR <sy870203>
2020-08-09 00:02:16 +0900

    add email check

--- routes/user.js
+++ routes/user.js
@@ -10,36 +10,62 @@ const userModel = require("../models/user");
 
 // 회원가입
 router.post("/signup", (req, res) => {
-    // bcrypt안에 hash함수 사용
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        if(err) {
-            return res.status(400).json({
-                error: err.message
-            });
-        } else {
-            const newUser = new userModel({
-                name: req.body.userName,
-                email: req.body.email,
-                password: hash
-            })
-        
-            newUser
-                .save()
-                .then(result => {
-                    console.log(result);
-                    res.status(200).json({
-                        message: 'User Created',
-                        userInfo: result
-                    })
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.status(500).json({
-                        error: err
-                    });
+    userModel
+        // 사용자가 입력한 email을 찾을 것이다.
+        .findOne({email: req.body.email})
+        .then(user => {
+            if(user){
+                return res.status(409).json({
+                    message: "Mail already exists"
                 });
-        }
-    })
+            } else {
+                //bcrypt안에 hash함수 사용
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                    if(err) {
+                        return res.status(400).json({
+                            error: err.message
+                        });
+                    } else {
+                        const newUser = new userModel({
+                            name: req.body.userName,
+                            email: req.body.email,
+                            password: hash
+                        })
+                        newUser
+                            .save()
+                            .then(result => {
+                                console.log(result);
+                                res.status(200).json({
+                                    message: 'User Created',
+                                    userInfo: result
+                                })
+                            })
+                            .catch(err => {
+                                console.log(err);
+                                res.status(500).json({
+                                    error: err
+                                });
+                            });
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err.message
+            })
+        })
+
+
+
+
+
+
+
+
+
+    
 
 
 
